73013159a7a1ac87006a498faae1d57b9134b1c1
luoxiaojun <luoxiaojun@dodoca.com>
2019-11-04 16:30:50 +0800

    add processor

---
+++ src/Query/Processors/Processor.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Lxj\Laravel\Presto\Query\Processors;
+
+class Processor extends \Illuminate\Database\Query\Processors\Processor
+{
+    /**
+     * Process the results of a column listing query.
+     *
+     * @param  array  $results
+     * @return array
+     */
+    public function processColumnListing($results)
+    {
+        return array_map(function ($result) {
+            return ((object) $result)->column_name;
+        }, $results);
+    }
+}
